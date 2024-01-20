b63ecf9bd57fd2bc8d4e13b1b0cf22cf20c37573
Duong Kim Nam <duongkimnam2142002@gmail.com>
2024-01-20 19:13:02 +0700

    connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+
+dotenv.config();
 const app = express();
+
+// database
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected to mongodb"))
+    .catch(err => console.log(err.message))
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+})
